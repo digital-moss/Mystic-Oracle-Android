043160164242dfd1f38360f3b0ea5f5da8ae83eb
digital-moss <me@handmeups.io>
2026-09-19 02:55:12 -0400

    build: bump version to 0.0.0.3
    Update versionCode and versionName for the latest release.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     applicationId = "com.aistudio.mysticoracle.qzx"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
-    versionName = "2.0"
+    versionCode = 3
+    versionName = "0.0.0.3"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
